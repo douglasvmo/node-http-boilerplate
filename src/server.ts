b3b3fdabc16da75714031c333b92945a77697634
Douglas <douglasolv.dev@gmail.com>
2020-09-17 20:24:49 -0300

    structure public files

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,39 @@
-import 'reflect-metadata';
-import './database/connection'
-import * as express from 'express'
-import routes from './routes'
-import { passport } from './app/controller/Authentication'
+import "reflect-metadata";
+import "./database/connection";
+import * as express from "express";
+import routes from "./routes";
+import { passport } from "./app/controller/Authentication";
 
-const app = express()
+const app = express();
 
 //handle template engine
-app.set('views', __dirname + '/app/views')
-app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'html')
+app.set("views", __dirname + "/app/views");
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
 // handle static assets
-app.use(express.static(__dirname + '/app/public'))
+app.use(express.static(__dirname + "/public"));
 
 //handle session and authentication
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(
+  require("express-session")({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 //handle form data body by post method
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: true,
-}))
+  })
+);
 
-app.use(routes)
+app.use(routes);
 
-
-app.listen(3000, () => console.log("🚀 Server started at http://localhost:3000"))
+app.listen(3000, () =>
+  console.log("🚀 Server started at http://localhost:3000")
+);
